b93adba5e9cf849487116d20aa9786ff3d365640
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-14 21:55:45 -0500

    pattern matching is very cool

---
+++ lib/lists/weather2.exs
@@ -0,0 +1,8 @@
+def WeatherHistory do
+  def for_location([], _target_loc), do: []
+
+  def for_location([[time, target_loc, temp, rain] | tail], target_loc) do
+    [[time, target_loc, temp, rain] | for_location(tail, target_loc)]
+  end
+  def for_location([ _ | tail ], target_loc) do: for_location(tail, target_loc)
+end
