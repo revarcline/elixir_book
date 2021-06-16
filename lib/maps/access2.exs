0e396ffea3a1a118330763fcefd6e1aae6a4194c
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-16 18:20:39 -0500

    know what to do

---
+++ lib/maps/access2.exs
@@ -0,0 +1,20 @@
+cast = [
+  %{
+    character: "Buttercup",
+    actor: {"Robin", "Wright"},
+    role: "princess"
+  },
+  %{
+    character: "Westley",
+    actor: {"Cary", "Elwes"},
+    role: "farm boy"
+  }
+]
+
+IO.inspect(get_in(cast, [Access.all(), :actor, Access.elem(1)]))
+
+IO.inspect(
+  get_and_update_in(cast, [Access.all(), :actor, Access.elem(1)], fn val ->
+    {val, String.reverse(val)}
+  end)
+)
---
+++ lib/maps/enum_funcs.exs
@@ -0,0 +1,22 @@
+defmodule MyEnum do
+  # all?(list, func)
+  # returns true if func is true for all elements
+
+  # each(list, func)
+  # invokes function for each element
+
+  # filter(list, func)
+  # only returns elements where func on value is true
+
+  # split(list, int)
+  # leave int elements in first list, return rest or empty
+
+  # take(list, int)
+  # return the first int elements for positive, last int elements for negative
+
+  # reverse(list)
+  # reverse elements in list
+
+  # flatten(list)
+  # puts all elements on top level of list
+end
