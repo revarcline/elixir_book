efb706f593709cab86100904cd8709c11abedafa
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-16 10:42:11 -0500

    hotel motel holiday inn

---
+++ lib/maps/book_room.exs
@@ -0,0 +1,17 @@
+defmodule HotelRoom do
+  def book(%{name: name, height: height})
+      when height > 1.9 do
+    IO.puts("Need extra-long bed for #{name}")
+  end
+
+  def book(%{name: name, height: height})
+      when height < 1.3 do
+    IO.puts("Need low shower controls for #{name}")
+  end
+
+  def book(person) do
+    IO.puts("Need regular bed for #{person.name}")
+  end
+end
+
+people |> Enum.each(&HotelRoom.book/1)
