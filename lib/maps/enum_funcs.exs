f03dda5622578d614dfd94042876de8597a43d66
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-16 21:58:29 -0500

    filter

--- lib/maps/enum_funcs.exs
+++ lib/maps/enum_funcs.exs
@@ -13,12 +13,32 @@ defmodule MyEnum do
 
   # each(list, func)
   # invokes function for each element
+  def each([], _func), do: :ok
+
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
 
   # filter(list, func)
   # only returns elements where func on value is true
+  def filter([], _func), do: []
+
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
 
   # split(list, int)
-  # leave int elements in first list, return rest or empty
+  # leave int elements in first list, return rest or empty. positive counts
+  # from front, negative from end
+  def split([], _int), do: []
+
+  def split([head | tail], int) do
+  end
 
   # take(list, int)
   # return the first int elements for positive, last int elements for negative
